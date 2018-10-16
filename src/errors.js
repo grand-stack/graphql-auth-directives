cacdf0d067032e1e5da4d420e3ac868e7e796fe4
johnymontana <lyonwj@gmail.com>
2018-10-15 19:10:54 -0600

    add errors

---
+++ src/errors.js
@@ -0,0 +1,7 @@
+import { createError } from "apollo-errors";
+
+const AuthorizationError = createError('AuthorizationError', {
+  message: 'You are not authorized.'
+});
+
+module.exports = { AuthorizationError };
